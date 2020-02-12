e4d2ef4e22c87cc0152148606c64748a7b4cb90b
Hudson D <hdoerr@vols.utk.edu>
2020-02-12 16:24:28 -0500

    Connects to required server, updates to .gitignore

--- bot.py
+++ bot.py
@@ -12,17 +12,11 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
+        f'{guild.name}(id: {guild.id})'
     )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
 
