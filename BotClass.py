66b18703765574a7374d8b00399e2097f5050a2e
Boostem <hdoerr@vols.utk.edu>
2020-03-04 10:26:00 -0500

    translates language, outputs to console.

---
+++ BotClass.py
@@ -0,0 +1,155 @@
+# bot1.py
+# A rewrite of bot.py, changing to the bot subclass from client superclass
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from googletrans import Translator
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='$')
+
+# Create new channels
+## NEEDS TO: determine admin permissions of the cmd caller, 
+# and check if the room being made already exists
+@bot.command(name='makechannel')
+#@commands.has_role('admin')
+async def makechannel(ctx, channel_name='Voltron-Conference'):
+	guild = ctx.guild
+	existing_channel = discord.utils.get(guild.channels, name=channel_name)
+	if not existing_channel:		
+		print(f'Creating new channel: {channel_name}')
+		await guild.create_text_channel(channel_name)
+
+
+#translate command 
+@bot.command(name='translate')
+async def translate(ctx, msg="ex phrase: $translate \"Hello!\" french" , dst='english'):
+    guild = ctx.guild
+    
+    #sets up translator object
+    translator = Translator()
+    
+    #A list made of a key-map for languages
+    LANGUAGES = {
+        'af': 'afrikaans',
+        'sq': 'albanian',
+        'am': 'amharic',
+        'ar': 'arabic',
+        'hy': 'armenian',
+        'az': 'azerbaijani',
+        'eu': 'basque',
+        'be': 'belarusian',
+        'bn': 'bengali',
+        'bs': 'bosnian',
+        'bg': 'bulgarian',
+        'ca': 'catalan',
+        'ceb': 'cebuano',
+        'ny': 'chichewa',
+        'zh-cn': 'chinese (simplified)',
+        'zh-tw': 'chinese (traditional)',
+        'co': 'corsican',
+        'hr': 'croatian',
+        'cs': 'czech',
+        'da': 'danish',
+        'nl': 'dutch',
+        'en': 'english',
+        'eo': 'esperanto',
+        'et': 'estonian',
+        'tl': 'filipino',
+        'fi': 'finnish',
+        'fr': 'french',
+        'fy': 'frisian',
+        'gl': 'galician',
+        'ka': 'georgian',
+        'de': 'german',
+        'el': 'greek',
+        'gu': 'gujarati',
+        'ht': 'haitian creole',
+        'ha': 'hausa',
+        'haw': 'hawaiian',
+        'iw': 'hebrew',
+        'hi': 'hindi',
+        'hmn': 'hmong',
+        'hu': 'hungarian',
+        'is': 'icelandic',
+        'ig': 'igbo',
+        'id': 'indonesian',
+        'ga': 'irish',
+        'it': 'italian',
+        'ja': 'japanese',
+        'jw': 'javanese',
+        'kn': 'kannada',
+        'kk': 'kazakh',
+        'km': 'khmer',
+        'ko': 'korean',
+        'ku': 'kurdish (kurmanji)',
+        'ky': 'kyrgyz',
+        'lo': 'lao',
+        'la': 'latin',
+        'lv': 'latvian',
+        'lt': 'lithuanian',
+        'lb': 'luxembourgish',
+        'mk': 'macedonian',
+        'mg': 'malagasy',
+        'ms': 'malay',
+        'ml': 'malayalam',
+        'mt': 'maltese',
+        'mi': 'maori',
+        'mr': 'marathi',
+        'mn': 'mongolian',
+        'my': 'myanmar (burmese)',
+        'ne': 'nepali',
+        'no': 'norwegian',
+        'ps': 'pashto',
+        'fa': 'persian',
+        'pl': 'polish',
+        'pt': 'portuguese',
+        'pa': 'punjabi',
+        'ro': 'romanian',
+        'ru': 'russian',
+        'sm': 'samoan',
+        'gd': 'scots gaelic',
+        'sr': 'serbian',
+        'st': 'sesotho',
+        'sn': 'shona',
+        'sd': 'sindhi',
+        'si': 'sinhala',
+        'sk': 'slovak',
+        'sl': 'slovenian',
+        'so': 'somali',
+        'es': 'spanish',
+        'su': 'sundanese',
+        'sw': 'swahili',
+        'sv': 'swedish',
+        'tg': 'tajik',
+        'ta': 'tamil',
+        'te': 'telugu',
+        'th': 'thai',
+        'tr': 'turkish',
+        'uk': 'ukrainian',
+        'ur': 'urdu',
+        'uz': 'uzbek',
+        'vi': 'vietnamese',
+        'cy': 'welsh',
+        'xh': 'xhosa',
+        'yi': 'yiddish',
+        'yo': 'yoruba',
+        'zu': 'zulu',
+        'fil': 'Filipino',
+        'he': 'Hebrew'
+    }
+    LANGCODES = dict(map(reversed, LANGUAGES.items()))
+    
+    #translator(phrase, destination lang, source language)
+    newlang = translator.translate(msg, LANGCODES[dst.lower()], src='auto')
+
+    print(newlang.text)
+
+
+
+bot.run(TOKEN)
