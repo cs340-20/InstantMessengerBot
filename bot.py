805afd37a8734532ec5251024715af7f62d8d88c
Tristen Finley <tfinley3@vols.utk.edu>
2020-03-04 19:25:11 -0500

    fixing command structure

--- bot.py
+++ bot.py
@@ -1,26 +1,190 @@
-# bot.py
-import os
+# bot1.py
+# A rewrite of bot.py, changing to the bot subclass from client superclass
 
+import os
 import discord
+
+from discord.ext import commands
 from dotenv import load_dotenv
+from googletrans import Translator
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+#
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
+# Welcome DM
+@bot.event
+async def on_member_join(member):
+	await member.create_dm()
+	await member.dm_channel.send(
+		f'VOLTRON WELCOMES YOU, {member.name}'
+	)
+
+@bot.event
+async def on_message(message):
+	if (message.author == bot.user):
+		return
+	await bot.process_commands(message)
+
+# Create new channels
+## NEEDS TO: determine admin permissions of the cmd caller, 
+# and check if the room being made already exist.
+
+@bot.command(name='makechannel')
+#@commands.has_role('admin')
+async def makechannel(ctx, channel_name='Voltron-Conference'):
+	guild = ctx.guild
+	existing_channel = discord.utils.get(guild.channels, name=channel_name)
+	if not existing_channel:		
+		print(f'Creating new channel: {channel_name}')
+		await guild.create_text_channel(channel_name)
+
+#translate command 
+@bot.command(name='translate')
+async def translate(ctx, msg="ex phrase: $translate \"Hello!\" french" , dst='english'):
+    
+    #sets up translator object
+    translator = Translator()
+    
+    #A list made of a key-map for languages
+    LANGUAGES = {
+        'af': 'afrikaans',
+        'sq': 'albanian',
+        'am': 'amharic',
+        'ar': 'arabic',
+        'hy': 'armenian',
+        'az': 'azerbaijani',
+        'eu': 'basque',
+        'be': 'belarusian',
+        'bn': 'bengali',
+        'bs': 'bosnian',
+        'bg': 'bulgarian',
+        'ca': 'catalan',
+        'ceb': 'cebuano',
+        'ny': 'chichewa',
+        'zh-cn': 'chinese (simplified)',
+        'zh-tw': 'chinese (traditional)',
+        'co': 'corsican',
+        'hr': 'croatian',
+        'cs': 'czech',
+        'da': 'danish',
+        'nl': 'dutch',
+        'en': 'english',
+        'eo': 'esperanto',
+        'et': 'estonian',
+        'tl': 'filipino',
+        'fi': 'finnish',
+        'fr': 'french',
+        'fy': 'frisian',
+        'gl': 'galician',
+        'ka': 'georgian',
+        'de': 'german',
+        'el': 'greek',
+        'gu': 'gujarati',
+        'ht': 'haitian creole',
+        'ha': 'hausa',
+        'haw': 'hawaiian',
+        'iw': 'hebrew',
+        'hi': 'hindi',
+        'hmn': 'hmong',
+        'hu': 'hungarian',
+        'is': 'icelandic',
+        'ig': 'igbo',
+        'id': 'indonesian',
+        'ga': 'irish',
+        'it': 'italian',
+        'ja': 'japanese',
+        'jw': 'javanese',
+        'kn': 'kannada',
+        'kk': 'kazakh',
+        'km': 'khmer',
+        'ko': 'korean',
+        'ku': 'kurdish (kurmanji)',
+        'ky': 'kyrgyz',
+        'lo': 'lao',
+        'la': 'latin',
+        'lv': 'latvian',
+        'lt': 'lithuanian',
+        'lb': 'luxembourgish',
+        'mk': 'macedonian',
+        'mg': 'malagasy',
+        'ms': 'malay',
+        'ml': 'malayalam',
+        'mt': 'maltese',
+        'mi': 'maori',
+        'mr': 'marathi',
+        'mn': 'mongolian',
+        'my': 'myanmar (burmese)',
+        'ne': 'nepali',
+        'no': 'norwegian',
+        'ps': 'pashto',
+        'fa': 'persian',
+        'pl': 'polish',
+        'pt': 'portuguese',
+        'pa': 'punjabi',
+        'ro': 'romanian',
+        'ru': 'russian',
+        'sm': 'samoan',
+        'gd': 'scots gaelic',
+        'sr': 'serbian',
+        'st': 'sesotho',
+        'sn': 'shona',
+        'sd': 'sindhi',
+        'si': 'sinhala',
+        'sk': 'slovak',
+        'sl': 'slovenian',
+        'so': 'somali',
+        'es': 'spanish',
+        'su': 'sundanese',
+        'sw': 'swahili',
+        'sv': 'swedish',
+        'tg': 'tajik',
+        'ta': 'tamil',
+        'te': 'telugu',
+        'th': 'thai',
+        'tr': 'turkish',
+        'uk': 'ukrainian',
+        'ur': 'urdu',
+        'uz': 'uzbek',
+        'vi': 'vietnamese',
+        'cy': 'welsh',
+        'xh': 'xhosa',
+        'yi': 'yiddish',
+        'yo': 'yoruba',
+        'zu': 'zulu',
+        'fil': 'Filipino',
+        'he': 'Hebrew'
+    }
+    LANGCODES = dict(map(reversed, LANGUAGES.items()))
+    
+    #translator(phrase, destination lang, source language)
+    newlang = translator.translate(msg, LANGCODES[dst.lower()], src='auto')
+
+    #print to console and print to server
+    print(newlang.text)
+    await ctx.send(newlang.text)
+
+@bot.event
+async def on_ready():
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 # Welcome DM
-@client.event
+@bot.event
 async def on_member_join(member):
 	await member.create_dm()
 	await member.dm_channel.send(
@@ -28,21 +192,4 @@ async def on_member_join(member):
 	)
 
 
-# Sends a volton message on Assemble!
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == '$assemble!':
-        response = 'LETS GO VOLTRON FORCE! ASSEMBLE!'
-        await message.channel.send(response)
-
-# Keep track of new users who join
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-
-
-client.run(TOKEN)
-
+bot.run(TOKEN)
